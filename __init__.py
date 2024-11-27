7fc321d77eea7273d4ca5bf6ad0aa71bec815af9
purpen <purpen.w@gmail.com>
2024-11-28 00:47:22 +0800

    add init code

---
+++ __init__.py
@@ -0,0 +1,9 @@
+from .translate import Translator
+
+NODE_CLASS_MAPPINGS = {
+    "Redoon_Translator": Translator
+}
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "Redoon_Translator": "Redoon Translator"
+}
---
+++ translate.py
@@ -0,0 +1,47 @@
+import requests
+
+class Translator:
+    def __init__(self) -> None:
+        pass
+    
+    @classmethod
+    def INPUT_TYPES(cls):
+        return {
+            "required": {
+                "input_text": ("Str", ),
+                "API_KEY": ("Str", ),
+            }
+        }
+        
+    RETURN_TYPES = ("Str",)
+    FUNCTION = "translate"
+    CATEGORY = "Redoon"
+    
+    def translate(self, input_text, api_key=None) -> tuple:
+        """
+        通过调用openai接口进行翻译,支持外网访问
+        """
+        if api_key:
+            trans_text = self.translate_by_openai(input_text=input_text)
+        else:
+            trans_text = self.translate_by_llama(input_text=input_text)
+        
+        return (trans_text, )
+    
+    
+    def translate_by_llama(self, input_text):
+        """
+        通过本地模型进行翻译
+        """
+        trans_text = ""
+        
+        return trans_text
+    
+    
+    def translate_by_openai(self, input_text) -> str:
+        """
+        通过openai接口进行翻译, 支持外网访问时可用
+        """
+        trans_text = ""
+        
+        return trans_text
